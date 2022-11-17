2df0ad7b3aa3996b4ea9ebb5b886c2c58eae7d41
Julia Petrenko <juliapetrenko02.07.1999@gmai.com>
2022-11-17 10:09:06 +0200

    second

--- goit-js-hw-06-main/src/js/task-09.js
+++ goit-js-hw-06-main/src/js/task-09.js
@@ -1,14 +1,16 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-
 const changeButton = document.querySelector('button.change-color')
 const body = document.querySelector('body')
 const colorValue = document.querySelector('span.color')
+
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
 
 changeButton.addEventListener('click', e => {
